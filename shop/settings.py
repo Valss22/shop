a5fbb9585fdbae339012d5b15fe8f393b3eb8bbb
Valss22 <valsshokorov@gmail.com>
2021-02-23 15:34:59 +0600

    success auth

--- shop/settings.py
+++ shop/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
 ROOT_URLCONF = 'shop.urls'
 
