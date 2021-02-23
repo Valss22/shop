87235f53f788350fd0725d438be6224b28c204fb
Valss22 <valsshokorov@gmail.com>
2021-02-23 18:37:06 +0600

    success auth

--- shop/settings.py
+++ shop/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000"
+]
+
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': (
 #         'rest_framework_simplejwt.authentication.JWTAuthentication',
