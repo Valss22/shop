ade8decfaad2ae8410f4ec7adba53b07570c96bf
Valss22 <valsshokorov@gmail.com>
2021-03-25 12:19:15 +0600

    fix cart

--- store/permissions.py
+++ store/permissions.py
@@ -20,17 +20,21 @@ class IsAuth(BasePermission):
 
 class FixInCart(BasePermission):
     def has_permission(self, request, view) -> bool:
-        access = request.headers['Authorization'].split(' ')[1]
-        access = parse_id_token(access)
+        try:
+            access = request.headers['Authorization'].split(' ')[1]
+            access = parse_id_token(access)
 
-        for i in list(Product.objects.all()):
-            Product.objects.filter(id=i.id).update(user=User.objects.get(email=access['email']))
+            for i in list(Product.objects.all()):
+                Product.objects.filter(id=i.id).update(user=User.objects.get(email=access['email']))
 
-            if UserProductRelation.objects.get(user=User.objects.get(email=access['email']),
-                                               product=Product.objects.get(id=i.id)).in_cart:
-                Product.objects.filter(user=User.objects.get(email=access['email']),
-                                       id=i.id).update(in_cart=True)
-            else:
-                Product.objects.filter(user=User.objects.get(email=access['email']),
-                                       id=i.id).update(in_cart=False)
-        return True
+                if UserProductRelation.objects.get(user=User.objects.get(email=access['email']),
+                                                   product=Product.objects.get(id=i.id)).in_cart:
+                    Product.objects.filter(user=User.objects.get(email=access['email']),
+                                           id=i.id).update(in_cart=True)
+                else:
+                    Product.objects.filter(user=User.objects.get(email=access['email']),
+                                           id=i.id).update(in_cart=False)
+            return True
+        except:
+            return True
+
