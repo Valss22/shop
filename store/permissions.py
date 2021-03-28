b0d52d15ddcd0cf604cdb695146e6ed199c8a94e
Valss22 <valsshokorov@gmail.com>
2021-03-28 16:03:33 +0600

    dadda

--- store/permissions.py
+++ store/permissions.py
@@ -24,12 +24,15 @@ class FixInCart(BasePermission):
             access = request.headers['Authorization'].split(' ')[1]
             access = parse_id_token(access)
             Product.objects.all().update(user=User.objects.get(email=access['email']))
-            for i in list(CartProduct.objects.all()):
-                if CartProduct.objects.get(user=User.objects.get(email=access['email']),
-                                           product=i.product).in_cart:
-                    Product.objects.filter(user=User.objects.get(email=access['email']),
-                                           id=i.product.id).update(in_cart=True)
+            try:
+                for i in list(CartProduct.objects.all()):
+                    if CartProduct.objects.get(user=User.objects.get(email=access['email']),
+                                               product=i.product).in_cart:
 
+                        Product.objects.filter(user=User.objects.get(email=access['email']),
+                                               id=i.product.id).update(in_cart=True)
+            except:
+                pass
             # for i in list(Product.objects.all()):
             #     Product.objects.filter(id=i.id).update(user=User.objects.get(email=access['email']))
             #
