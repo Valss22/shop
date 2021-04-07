2beda32d72dd8a0286b32fcb91c6dad4d2491583
Valss22 <valsshokorov@gmail.com>
2021-04-07 11:39:56 +0600

    add img

--- shop/urls.py
+++ shop/urls.py
@@ -37,13 +37,13 @@ urlpatterns = [
 
 urlpatterns += router.urls
 
-from django.conf import settings
-from django.urls import include, path
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [
-
-                      path('__debug__/', include(debug_toolbar.urls)),
-                  ] + urlpatterns
+# from django.conf import settings
+# from django.urls import include, path
+#
+# if settings.DEBUG:
+#     import debug_toolbar
+#
+#     urlpatterns = [
+#
+#                       path('__debug__/', include(debug_toolbar.urls)),
+#                   ] + urlpatterns
